819dd4bc0d87b1f40ff55cb7363cbbb927ed882d
sherms33 <sher0806@gmail.com>
2015-09-06 19:51:09 -0600

    Create Exercise2-3.py

---
+++ Exercise2-3.py
@@ -0,0 +1,9 @@
+#Exercise 2.3
+#Enter Hours: 35
+#Enter Rate: 2.75
+#Pay: 96.25
+
+hours_worked = int(raw_input('Enter hours worked: '))
+pay_rate = float(raw_input('Enter pay rate: '))
+total_pay = hours_worked * pay_rate
+ print 'Pay: ' + str(total_pay)
