aa01a037f9ef88c7ab6c606027332fcb49ba3c30
sherms33 <sher0806@gmail.com>
2015-09-08 19:30:57 -0600

    Update Exercise3-1.py

--- Exercise3-1.py
+++ Exercise3-1.py
@@ -11,11 +11,13 @@
 
 hours_worked = int(raw_input('Enter hours worked: '))
 pay_rate = float(raw_input('Enter pay rate: '))
-if hours_worked <= 40
- total_pay = hours_worked * pay_rate
-else
- overtime_hours = hours_worked - 40
- total_pay = 40 * pay_rate
 
+if hours_worked <= 40:
+ total_pay = hours_worked * pay_rate
+else:
+ overtime_hours = hours_worked - 40
+ overtime_payrate = pay_rate * 1.5
+ overtime_pay = ovetime_hours *overtime_payrate
+ total_pay = 40 * pay_rate + overtime_pay
 
 print 'Pay: ' + str(total_pay)
